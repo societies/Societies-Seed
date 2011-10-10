d34c5d0d69f25c87dfa74c63b52afa04e6e733d9
pkuppuud <kperumalin@hotmail.com>
2011-10-10 17:09:16 +0100

    Spring OSGI (Virgo) version of FortuneCookieXC project

---
+++ FortuneCookieXC.Virgo/src/main/java/org/socialblend/fortunecookies/xc/Main.java
@@ -0,0 +1,42 @@
+package org.socialblend.fortunecookies.xc;
+
+import org.jivesoftware.whack.ExternalComponentManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xmpp.component.ComponentException;
+
+public class Main {
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+//	public static void main(String[] arg) {
+//		new Main();
+//	}
+	
+	public Main() {
+	
+		System.out.println(" *** fortunecookies.xc bean started ***");
+		
+		org.xmpp.component.Component fortuneCookieComp = new FortuneCookieComponent();
+		ExternalComponentManager manager = new ExternalComponentManager(
+				"red.local");
+		manager.setSecretKey("fortunecookies.red.local", "password.red.local");
+
+		log.info("Connected!");	
+		try {
+			manager.addComponent("fortunecookies.red.local",
+					fortuneCookieComp);
+		} catch (ComponentException e) {
+			e.printStackTrace();
+		}
+
+		while (true) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
