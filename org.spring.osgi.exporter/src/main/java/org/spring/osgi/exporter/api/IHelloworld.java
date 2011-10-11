9ddf96dcbb1bc454f8f53511a239ad8f9a3d7c39
pkuppuud <kperumalin@hotmail.com>
2011-10-11 16:20:20 +0100

    example project structure changed

--- org.spring.osgi.exporter/src/main/java/org/spring/osgi/exporter/api/IHelloworld.java
+++
@@ -1,7 +0,0 @@
-package org.spring.osgi.exporter.api;
-
-public interface IHelloworld {
-
-	public String echoHellowWorld();
-	
-}
--- osgi.plain.exporter/src/main/java/osgi/plain/exporter/api/TimesTwoService.java
+++
@@ -1,5 +0,0 @@
-package osgi.plain.exporter.api;
-
-public interface TimesTwoService {
-	int multiply(int input);
-}
